6e0c88814d04d8c5db30cd82f224d5d8f290064c
Cory Monty <cory.monty@gmail.com>
2011-01-17 19:42:48 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cory::Application.config.secret_token = 'cf5a1d8b26f3b1b95fb7391d9b36678d9b1862c3573bc9b963f322e7e0ab3e343044f88b9798a6a4f872121b0096b69131544c81a513564de7ce79ef723a339e'
