1d2ee957568ab4c4187695e5d26f057ffeac3793
Cory Monty <cory.monty@gmail.com>
2011-01-20 20:01:44 -0500

    Adds posts

---
+++ db/migrate/20110121004640_add_posts.rb
@@ -0,0 +1,17 @@
+class AddPosts < ActiveRecord::Migration
+  def self.up
+    create_table :posts do |t|
+      t.primary_key :id
+      t.text :body
+      t.string :title
+      t.string :image
+      t.string :caption
+      
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :posts
+  end
+end
