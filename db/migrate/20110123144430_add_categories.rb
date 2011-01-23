00ae6c311e6d193304253904a7637d7fa0129e52
Cory Monty <cory.monty@gmail.com>
2011-01-23 09:45:47 -0500

    Adds categories table

---
+++ db/migrate/20110123144430_add_categories.rb
@@ -0,0 +1,15 @@
+class AddCategories < ActiveRecord::Migration
+  def self.up
+    create_table :categories do |t|
+      t.string :title
+      t.string :slug
+      t.string :category_type
+
+      t.timestamps
+    end  
+  end
+
+  def self.down
+    drop_table :categories
+  end
+end
