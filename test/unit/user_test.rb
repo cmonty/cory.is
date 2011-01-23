0cf3e05e7c0aa67a2142001749675993e4c0ba29
Cory Monty <cory.monty@gmail.com>
2011-01-23 14:30:25 -0500

    Basic unit test to check for valid password

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "valid_password" do
+    assert users(:cory).password.length == 128
   end
 end
