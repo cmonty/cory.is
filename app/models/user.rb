7f11e3d674f9a71b7f65bca579451fb5ff7511c1
Cory Monty <cory.monty@gmail.com>
2011-01-24 18:13:58 -0500

    Finalizes user creation unit test, adds validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  attr_accessor :new_password, :new_password_confirmation
+  
+  validates_confirmation_of :new_password
+  validates_presence_of :name, :email 
+  before_save :hash_password
+  
+  def hash_password
+    time = Time.now.to_s
+    self.password = Digest::SHA512.hexdigest("#{@new_password}:#{time}")
+  end 
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "valid_password" do
-    assert users(:cory).password.length == 128
+  test "should hash password" do
+    user = User.new :name => 'Cory',
+                    :email => 'hi@cory.is',
+                    :new_password => 'unlock2',
+                    :new_password_confirmation => 'unlock2'
+    assert user.save
   end
 end
