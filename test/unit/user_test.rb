c50fe9318663f3e30409afab3936aa2183d54f67
Cory Monty <cory.monty@gmail.com>
2011-01-24 18:16:26 -0500

    Adds messages to failed user tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+  
   test "should hash password" do
     user = User.new :name => 'Cory',
                     :email => 'hi@cory.is',
                     :new_password => 'unlock2',
                     :new_password_confirmation => 'unlock2'
-    assert user.save
-    assert user.password.length == 128
+    assert user.save, "Failed to create user"
+    assert user.password.length == 128, "Failed to hash password"
   end
+  
 end
