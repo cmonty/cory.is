2d89f4ae3f012772e68e6a536141750708ab3ffb
Cory Monty <cory.monty@gmail.com>
2011-01-24 18:34:06 -0500

    Line break

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   attr_accessor :new_password, :new_password_confirmation
   
   validates_confirmation_of :new_password
-  validates_presence_of :name, :email 
+  validates_presence_of :name, :email
+   
   before_save :hash_password
   
   def hash_password
