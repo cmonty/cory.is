7f03114f8c9425f334b7314e26b6ecc4f0dbfd39
Cory Monty <cory.monty@gmail.com>
2011-01-26 19:59:32 -0500

    Unit test for authenticating users

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -11,4 +11,8 @@ class UserTest < ActiveSupport::TestCase
     assert user.password.length == 128, "Failed to hash password"
   end
   
+  test "should authenticate" do
+    assert_instance_of User, User.authenticate('hi@cory.is', 'unlock2'), "Failed to authenticate user"
+  end
+  
 end
