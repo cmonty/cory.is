bb50f51142828671a1b45393e68b14ef25b6df96
Cory Monty <cory.monty@gmail.com>
2011-02-05 11:26:10 -0500

    Adds post edit view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,4 +19,12 @@ class PostsController < ApplicationController
     
   end
   
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
 end
