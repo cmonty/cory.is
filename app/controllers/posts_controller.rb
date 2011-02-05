1f5a6603e6ad3b4c7c8a7fdd851c39fc24c7cf6e
Cory Monty <cory.monty@gmail.com>
2011-02-05 09:16:34 -0500

    Writes unit tests for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  
+  def index
+    posts = Post.all
+  end
+  
+  def new
+    post = Post.new
+  end
+  
+  def create
+    
+  end
+  
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should not create post without body" do
+    post = Post.new :title => "Hello World" 
+    assert !post.save, "Created post without body"
+  end
+  
+  test "should not create post without title" do
+    post = Post.new :body => "Mustache guy"
+    assert !post.save, "Created post without title"
+  end
+  
+  test "should create post" do
+    post = Post.new :title => "Hello World!",
+                    :body => "Mustache guy"
+    assert post.save, "Failed to create post"
   end
 end
