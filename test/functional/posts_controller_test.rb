35a61a959f3d5998ffa568ea04ee25ca772d6632
Cory Monty <cory.monty@gmail.com>
2011-02-06 10:57:09 -0500

    Functional tests for photo upload

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "image upload" do
+    test_image = "/Users/Cory/Sites/cory/test/fixtures/files/test_upload.jpg"
+    file = Rack::Test::UploadedFile.new(test_image, "image/jpeg")
+    assert_difference('Post.count') do
+      post :create, :post => { :photo => file }
+    end
   end
 end
