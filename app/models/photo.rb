a5fd4146ef44f036824ab7ddcf3f3f1ea3978d12
Cory Monty <cory.monty@gmail.com>
2011-02-05 21:33:00 -0500

    Removes photo model

--- app/models/photo.rb
+++
@@ -1,5 +0,0 @@
-class Photo < ActiveRecord::Base
-  
-  belongs_to :post
-  
-end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ActiveRecord::Base
   
-  has_many :photos
   
   validates_presence_of :title, :body
   
-  accepts_nested_attributes_for :photos
   
 end
