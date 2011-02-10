44628370d16945a15d120a6a878c2061b9ae496f
Cory Monty <cory.monty@gmail.com>
2011-02-09 19:46:49 -0500

    Adds nested form elements for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :posts
   
   validates_presence_of :title, :type
   
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-  has_many :categories
+  has_and_belongs_to_many :categories
+  accepts_nested_attributes_for :categories
   
   has_attached_file :photo
   validates_presence_of :title, :body
