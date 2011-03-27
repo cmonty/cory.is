c66b05015bf21db8699db3bc8ed8795b38cafbb4
Cory Monty <cory.monty@gmail.com>
2011-03-26 20:03:44 -0400

    Adds method for creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     
   end
   
+  def create
+    @user = User.new(params[:user])
+    
+    if @user.save
+      redirect_to new_post_url
+    else
+      render :action => "new"
+    end
+  end
+  
 end
