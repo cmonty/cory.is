dd8477a40bb762d232f0221363b765b914e90bd4
Cory Monty <cory.monty@gmail.com>
2011-04-23 14:01:05 -0400

    Configures production databse

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "cory.is"
+set :repository,  "git@github.com:cmonty/Cory.is.git"
 
-set :scm, :subversion
+set :deploy_to, "/home/cory/public_html/cory.is"
+set :user, "cory"
+set :port, 30000
+set :ssh_options, { :forward_agent => true }
+set :deploy_via, :remote_cache
+set :copy_strategy, :checkout
+set :keep_releases, 3
+set :use_sudo, false
+set :copy_compression, :bz2
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server "cory.is", :app, :web, :db, :primary => true
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
